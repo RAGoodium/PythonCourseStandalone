946b97921d8efe956559101363762d14451999cf
ragoodium <d.b.sambuev@gmail.com>
2024-11-06 17:18:18 +0800

    ch_6, sl_9.

--- course/chapter_06/solutions/solution_9.py
+++ course/chapter_06/solutions/solution_9.py
@@ -1,5 +1,19 @@
-w = 4 #int(input())
-h = 5 #int(input())
+w = int(input())
+h = int(input())
+t = 0
 
-for i in range(w):
-    print(i + 4, "\t", end="")
+k = h * w
+max_len = len(str(k))
+
+for u in range (1, h):
+    for i in range(w + 1):
+        num = u + i * h - i
+        num_len = len(str(num)) - 1
+        s = " " * (max_len - num_len)
+        
+        if num == k - i + u: 
+            print(num, end="", sep="")
+        else:
+            print(num, s, end="", sep="")
+    print()
+    
