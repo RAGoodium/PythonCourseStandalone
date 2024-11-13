bf38baf19337d8c3c31ee68f8282df165516934c
ragoodium <d.b.sambuev@gmail.com>
2024-11-13 17:19:25 +0800

    ch_6, sl_10 up-2.

--- course/chapter_06/solutions/solution_10.py
+++ course/chapter_06/solutions/solution_10.py
@@ -1,12 +1,18 @@
 n = 5 #int(input())
 
-for i in range(1, n + 1):
-    for k in range(1, n + 2):
+for i in range(n + 1):
+    for k in range(n + 1):
 
+        sl = min(i, k, n - i, n - k) + 1
+                 
         if k > i:
-            print(i, '\t', end="", sep="")
+            if k == n:
+                print(sl, end="", sep="")
+            else:
+                print(sl, end=" ", sep="")
         elif k < i:
-            print(k, '\t', end="", sep="")
-            
-
+            if k == n - 1:
+                print(sl, end="", sep="")
+            else:
+                print(sl, end=" ", sep="")
     print()
