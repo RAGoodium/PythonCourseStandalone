997f676f60b73b468e1f8f4b1f2df90358a5a123
ragoodium <d.b.sambuev@gmail.com>
2024-11-15 17:04:09 +0800

    ch_6, sl_10 up-3. ch_7, sl_1-2.

--- course/chapter_06/solutions/solution_10.py
+++ course/chapter_06/solutions/solution_10.py
@@ -1,18 +1,22 @@
-n = 5 #int(input())
+n = int(input())
 
 for i in range(n + 1):
     for k in range(n + 1):
 
-        sl = min(i, k, n - i, n - k) + 1
-                 
-        if k > i:
-            if k == n:
-                print(sl, end="", sep="")
-            else:
-                print(sl, end=" ", sep="")
-        elif k < i:
-            if k == n - 1:
-                print(sl, end="", sep="")
-            else:
-                print(sl, end=" ", sep="")
-    print()
+        sl = min(n - i, n - k, i, k) + 1
+        
+        if i == n // 2:
+            continue
+        else:
+         
+            if k > i:
+                if k == n:
+                    print(sl, end="\n", sep="")
+                else:
+                    print(sl, end=" ", sep="")
+            elif k < i:
+                if k == n - 1:
+                    print(sl, end="\n", sep="")
+                else:
+                    print(sl, end=" ", sep="")
+
---
+++ course/chapter_07/solutions/solution_1.py
@@ -0,0 +1,5 @@
+numbers = [10, 20, 30, 40, 50]
+summ = 0
+for n in range(len(numbers)):
+    summ = summ + numbers[n]
+print(f'Сумма элементов списка: {summ}')
