3370285d1ae20c299139385364deb24246e7a2dc
ragoodium <d.b.sambuev@gmail.com>
2025-01-17 17:16:35 +0800

    ch_11, sl_3.

--- course/chapter_11/solutions/solution_3.py
+++ course/chapter_11/solutions/solution_3.py
@@ -1,2 +1,14 @@
-n = "Ольга: 6"
-print(n.split())
+numbers = input().split()
+target = int(input())
+done = 0
+for n in numbers:
+    n = int(n)
+    minus = target - n
+    answer = (n, minus)
+    if str(minus) in numbers:
+        print(answer)
+        done +=1
+        break
+if done == 0:
+    print(target)
+    
