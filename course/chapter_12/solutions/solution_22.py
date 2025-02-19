9b00705d318599d04435e1b9edb1714781998265
ragoodium <d.b.sambuev@gmail.com>
2025-02-19 17:14:37 +0800

    ch_12, sl_22.

--- course/chapter_12/solutions/solution_22.py
+++ course/chapter_12/solutions/solution_22.py
@@ -1,17 +1,29 @@
 def check_analysis(**checks):
-    amount = dict()
+
+    goods = list()
+    amount = list()
+    sum = 0
+    ids = list()
+    prices = list()
+    for id, check in checks.items():
+        for item, price in check.items():
+            goods.append(item)
+            sum += price
+        ids.append(id)
+        prices.append(sum)
+        sum = 0
+    mx = max(prices)
+    place_id = prices.index(mx)
+            
     for check in checks.values():
         for item in check:
-            if item not in amount:
-                amount[item] = 1
-            else:
-                amount[item] += 1
-    hwmch = list()
-    for kolvo in amount.values():
-        hwmch.append(kolvo)
-        mx = max(hwmch)
-    print(amount)
-
+            amount.append(goods.count(item))
+    mx = max(amount)
+    place_item = amount.index(mx)
+    
+    return (ids[place_id], goods[place_item])
+    
+    
 check_analysis(
     id123={"яблоко": 100, "банан": 50},
     id124={"банан": 50, "апельсин": 70},
