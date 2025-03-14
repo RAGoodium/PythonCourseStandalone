54b214502f32eb22815a34d235c3ec3e7015d950
ragoodium <d.b.sambuev@gmail.com>
2025-03-14 18:15:31 +0800

    fix ch_9, sl_15. | ALL COMPLETE!

--- course/chapter_09/solutions/solution_15.py
+++ course/chapter_09/solutions/solution_15.py
@@ -1,3 +1,10 @@
-n = input()
-
-print(n.title())
+n = input().split()
+answ = list()
+for word in n:
+    if word.isalpha() == True:
+        answ.append(word.title())
+    else:
+        answ.append(word)
+        
+                            #привет мир это python3
+print('Результат:', " ".join(answ)) #Результат: Привет Мир Это python3
